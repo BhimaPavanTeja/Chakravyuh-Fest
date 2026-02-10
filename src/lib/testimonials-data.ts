2fb879ecc4a8503c87f1ddb8f8996deb0676e66f
Bhima Pavan Teja <pavantejabhima@gmail.com>
2026-02-10 05:08:45 +0000

    updated testimonials

--- src/lib/testimonials-data.ts
+++ src/lib/testimonials-data.ts
@@ -1,5 +1,23 @@
 export const testimonials = [
   {
+    quote: "As the Overall Chairperson of Chakravyuh Fest 205 I demonstrated exceptional leadership and organizational skills. My ability to coordinate multiple teams, handle challenges calmly, and ensure smooth execution of events played a crucial role in the success of the fest.",
+    name: "K.Raghavendra",
+    role: "Chairperson",
+    imageUrl: "/testimonials/exchair.jpeg",
+  },
+  {
+    quote: "The core team organized a successful event, overcoming challenges while ensuring enjoyment. As the head of sponsorship, I acknowledge the vital support of my team and staff, expressing hope for the event to become an annual tradition.",
+    name: "Nithin Narayana",
+    role: "Sponsors head",
+    imageUrl: "/testimonials/NithinNarayana.jpeg",
+  },
+  {
+    quote: "As the Sponsorship and Accounts lead for Chakravyuh Fest 2025, I played a key role in securing sponsors and managing finances efficiently. My professional approach and clear communication ensured smooth collaboration with partners and accurate handling of budgets.",
+    name: "Reddy Harsha Vardhan",
+    role: "Accounts and sponsor",
+    imageUrl: "/testimonials/harsha.jpeg",
+  },
+  {
     quote: "As the Resource Management Head for Chakravyuh, I am proud of the scale and excellence our team achieved. From overseeing event infrastructure, branding, décor, certification workflows, and end-to-end logistics to ensuring seamless participant services, we transformed meticulous planning into flawless execution.",
     name: "Ravindranadh",
     role: "Cyber security",
@@ -8,7 +26,7 @@ export const testimonials = [
   {
     quote: "As a Core Team Member – Resource Management, what began as a simple idea slowly turned into a beautiful reality. Playing a major role in the entire college decoration for Chakravyuh 2K25, I witnessed firsthand the effort, pressure, and passion behind every detail that brought the campus to life.",
     name: "Ruttala Yaswanth",
-    role: "EEE",
+    role: "Resource Management",
     imageUrl: "/testimonials/RuttalaYaswanth.jpeg",
   },
     {
@@ -20,13 +38,7 @@ export const testimonials = [
   {
     quote: "As the Treasurer(Accountant) of the Chakravyuh 2K25, I take pride in the effective financial planning and execution that supported the smooth conduct of the fest. With the cooperation and trust of the entire team, resources were managed responsibly to ensure the success of all events.",
     name: "S.Giri Charan Naidu",
-    role: "Mechanical",
+    role: "Treasurer",
     imageUrl: "/testimonials/GiriCharanNaidu.jpeg",
-  },
-  {
-    quote: "The core team organized a successful event, overcoming challenges while ensuring enjoyment. As the head of sponsorship, I acknowledge the vital support of my team and staff, expressing hope for the event to become an annual tradition.",
-    name: "Nithin Narayana",
-    role: "Sponsors head",
-    imageUrl: "/testimonials/NithinNarayana.jpeg",
   }
 ];
